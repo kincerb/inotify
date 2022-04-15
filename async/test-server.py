a5d48d0afef4f48361a161ae2852aef75cbf2c9e
kincerb <dev.bkincer@gmail.com>
2022-04-15 12:04:03 -0400

    Black formatting.

--- async/test-server.py
+++ async/test-server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import argparse
 import asyncio
-import functools
 import logging
 import logging.handlers
 import signal
@@ -17,6 +16,7 @@ logger = logging.getLogger(__name__)
 
 class ServerError(Exception):
     """Exception class for this script."""
+
     pass
 
 
@@ -24,7 +24,7 @@ def handler(sig):
     loop = asyncio.get_running_loop()
     for task in asyncio.all_tasks(loop=loop):
         task.cancel()
-    logger.debug(f'Signal: {sig!s} caught, shutting down.')
+    logger.debug(f"Signal: {sig!s} caught, shutting down.")
     loop.remove_signal_handler(signal.SIGTERM)
     loop.add_signal_handler(signal.SIGINT, lambda: None)
 
@@ -42,15 +42,15 @@ class SocketServerState:
         await self._on_connect(writer)
 
     async def _on_connect(self, writer: StreamWriter) -> None:
-        logger.info(f'New client connected, total of {len(self._writers)} user(s).')
-        writer.write('You are now subscribed to inotify events.'.encode())
+        logger.info(f"New client connected, total of {len(self._writers)} user(s).")
+        writer.write("You are now subscribed to inotify events.".encode())
         await writer.drain()
 
     async def _monitor_queue(self) -> None:
         while True:
             await asyncio.sleep(1)
             event = await self._events.get()
-            logger.debug('Received event off the queue, notifying clients.')
+            logger.debug("Received event off the queue, notifying clients.")
             await self._notify_all(str(event))
 
     async def _notify_all(self, event_payload: str) -> None:
@@ -60,15 +60,15 @@ class SocketServerState:
                 await writer.drain()
             except ConnectionError:
                 self._writers.remove(writer)
-                logger.debug(f'Client removed, total of {len(self._writers)} user(s).')
+                logger.debug(f"Client removed, total of {len(self._writers)} user(s).")
 
     async def monitor_paths(self, paths: List[str]) -> None:
         with Inotify() as inotify:
             for path in paths:
-                logger.debug(f'Adding watch for {path}.')
+                logger.debug(f"Adding watch for {path}.")
                 inotify.add_watch(path, Mask.ATTRIB | Mask.ACCESS | Mask.OPEN)
             async for event in inotify:
-                logger.debug(f'Adding {event} to the queue.')
+                logger.debug(f"Adding {event} to the queue.")
                 await self._events.put(event)
 
 
@@ -80,7 +80,7 @@ async def main(args: argparse.Namespace) -> None:
     try:
         setup_server_socket(args.socket)
     except ServerError as e:
-        logger.critical('Failed to setup socket for server.', exc_info=e)
+        logger.critical("Failed to setup socket for server.", exc_info=e)
         sys.exit(1)
 
     server_state = SocketServerState(args)
@@ -93,7 +93,7 @@ async def main(args: argparse.Namespace) -> None:
         async with server:
             await server.serve_forever()
     except asyncio.CancelledError:
-        logger.info('Server has shut down.')
+        logger.info("Server has shut down.")
 
 
 def setup_server_socket(socket_path: Path) -> None:
@@ -111,31 +111,36 @@ def get_args() -> argparse.Namespace:
     """Gather arguments for the script."""
 
     parser = argparse.ArgumentParser(
-        description='Server to publish inotify events',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('paths',
-                        nargs='*',
-                        default=['/usr/bin/python3', '/home/kincerb'],
-                        help='Filesystem paths to monitor.')
-    parser.add_argument('-s', '--socket',
-                        required=False,
-                        dest='socket',
-                        default='/var/tmp/inotify.sock',
-                        type=Path,
-                        help='Socket to use for server.')
-    parser.add_argument('-v', '--verbose',
-                        required=False,
-                        dest='verbosity',
-                        action='count',
-                        default=0,
-                        help='Increase output verbosity.')
+        description="Server to publish inotify events", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "paths", nargs="*", default=["/usr/bin/python3", "/home/kincerb"], help="Filesystem paths to monitor."
+    )
+    parser.add_argument(
+        "-s",
+        "--socket",
+        required=False,
+        dest="socket",
+        default="/var/tmp/inotify.sock",
+        type=Path,
+        help="Socket to use for server.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        required=False,
+        dest="verbosity",
+        action="count",
+        default=0,
+        help="Increase output verbosity.",
+    )
     return parser.parse_args()
 
 
 def setup_logging(verbosity: int = 0) -> None:
     """Configures global logging object for the script."""
     level = logging.INFO if verbosity == 0 else logging.DEBUG
-    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
 
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(level)
@@ -150,11 +155,11 @@ def setup_logging(verbosity: int = 0) -> None:
         handlers=[
             stdout_handler,
             stderr_handler,
-        ]
+        ],
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
     setup_logging(verbosity=args.verbosity)
     asyncio.run(main(args))
